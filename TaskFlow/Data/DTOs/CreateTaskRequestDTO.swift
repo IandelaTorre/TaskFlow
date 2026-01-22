0fffd0e2d1ea4e6a302bd546b6a7398962a797dc
Ian Perez <ianaxelperezdelatorre@gmail.com>
2026-01-22 00:14:42 -0600

    Starting with the API Request implementation for Tasks.

---
+++ TaskFlow/Data/DTOs/CreateTaskRequestDTO.swift
@@ -0,0 +1,8 @@
+//
+//  CreateTaskRequestDTO.swift
+//  TaskFlow
+//
+//  Created by Ian Axel Perez de la Torre on 21/01/26.
+//
+
+import Foundation
---
+++ TaskFlow/Data/Network/TaskAPI.swift
@@ -0,0 +1,8 @@
+//
+//  TaskAPI.swift
+//  TaskFlow
+//
+//  Created by Ian Axel Perez de la Torre on 21/01/26.
+//
+
+import Foundation
