9d3defa4ac4180a841907bf775b598fa8c98f4e4
Ian Perez <ianaxelperezdelatorre@gmail.com>
2026-01-28 00:12:32 -0600

    Now all the request are implemented in the app. The last one of create task now is implemented on the addTask screen. Now I need to start with several improvements. In the pr i would list all that I remember now.

--- TaskFlow/Presentation/Modules/Home/AddTaskViewController.swift
+++ TaskFlow/Presentation/Modules/Home/AddTaskViewController.swift
@@ -6,14 +6,63 @@
 //
 
 import UIKit
+import Combine
 
 class AddTaskViewController: UIViewController {
     
+    @IBOutlet weak var assignToTextField: UITextField!
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var descriptionTextField: UITextField!
+    @IBOutlet weak var createTaskButton: UIButton!
+    @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
+    
     var viewModel: HomeViewModel!
+    
+    private var cancellables: Set<AnyCancellable> = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        bind()
+        viewModel.loadUser()
 
+    }
+    
+    private func bind() {
+        loadingIndicator.hidesWhenStopped = true
+        viewModel.$isLoading
+            .receive(on: RunLoop.main)
+            .sink { [weak self] loading in
+                if loading { self?.loadingIndicator.startAnimating() }
+                else { self?.loadingIndicator.stopAnimating() }
+            }
+            .store(in: &cancellables)
+        
+    }
+    
+    @IBAction func CreateTaskButtonAction(_ sender: Any) {
+        createTaskButton.isEnabled = false
+        
+        
+        defer {
+            createTaskButton.isEnabled = true
+        }
+        
+        Task { @MainActor in
+            if let assignTo = assignToTextField.text, let title = titleTextField.text, let description = descriptionTextField.text, let assignedBy = viewModel.user?.userCode {
+                let create = await self.viewModel.createTask(title: title, description: description, statusId: 1, assignedTo: assignTo, assignedBy: assignedBy)
+                if create {
+                    self.showToast(message: "Tarea actualizada correctamente. ", seconds: 3.0)
+                }
+            } else {
+                self.showToast(message: "Alguno de los campos no esta cargando correctamente.", seconds: 3.0)
+            }
+        }
+    }
+    
+    @IBAction func UsersListButtonAction(_ sender: Any) {
+    }
+    
+    @IBAction func AssignToMeButtonAction(_ sender: Any) {
     }
     
 }
